f2dc4b74da8345dea5dea99d23b3d325547e750d
mano <ma@no.com>
2026-04-23 15:09:24 +0200

    betty

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,15 +1,22 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_node - prints
+ * @parent: tree
+ * @value: val
+ *
+ * Return: Always 0 (Success)
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-    if (!new)
-        return (NULL);
+	if (!new)
+		return (NULL);
 
-    new->parent = parent;
-    new->n = value;
-    new->left = NULL;
-    new->right = NULL;
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
 
-    return new;
+	return (new);
 }
--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,84 +1,106 @@
 #include "binary_trees.h"
-
+/**
+ * tree_size - prints
+ * @tree: tree
+ *
+ * Return: Always 0 (Success)
+ */
 static size_t tree_size(const heap_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    return (1 + tree_size(tree->left) + tree_size(tree->right));
+	return (1 + tree_size(tree->left) + tree_size(tree->right));
 }
-
+/**
+ * node_at_index - prints
+ * @root: tree
+ * @index: index
+ *
+ * Return: Always 0 (Success)
+ */
 static heap_t *node_at_index(heap_t *root, size_t index)
 {
-    size_t bit;
+	size_t bit;
 
-    if (!root || index == 0)
-        return (NULL);
+	if (!root || index == 0)
+		return (NULL);
 
-    for (bit = 1; bit <= index; bit <<= 1)
-        ;
-    bit >>= 2;
+	for (bit = 1; bit <= index; bit <<= 1)
+		;
+	bit >>= 2;
 
-    while (bit && root)
-    {
-        if (index & bit)
-            root = root->right;
-        else
-            root = root->left;
-        bit >>= 1;
-    }
+	while (bit && root)
+	{
+		if (index & bit)
+			root = root->right;
+		else
+			root = root->left;
+		bit >>= 1;
+	}
 
-    return (root);
+	return (root);
 }
-
+/**
+ * sift_up - prints
+ * @node: tree
+ *
+ * Return: Always 0 (Success)
+ */
 static heap_t *sift_up(heap_t *node)
 {
-    int tmp;
+	int tmp;
 
-    while (node && node->parent && node->n > node->parent->n)
-    {
-        tmp = node->n;
-        node->n = node->parent->n;
-        node->parent->n = tmp;
-        node = node->parent;
-    }
+	while (node && node->parent && node->n > node->parent->n)
+	{
+		tmp = node->n;
+		node->n = node->parent->n;
+		node->parent->n = tmp;
+		node = node->parent;
+	}
 
-    return (node);
+	return (node);
 }
-
+/**
+ * heap_insert - prints
+ * @root: tree
+ * @value: val
+ *
+ * Return: Always 0 (Success)
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *parent, *new_node;
-    size_t new_index;
+	heap_t *parent, *new_node;
+	size_t new_index;
 
-    if (!root)
-        return (NULL);
+	if (!root)
+		return (NULL);
 
-    if (!*root)
-    {
-        *root = binary_tree_node(NULL, value);
-        if (!*root)
-            return (NULL);
-        (*root)->left = NULL;
-        (*root)->right = NULL;
-        return (*root);
-    }
+	if (!*root)
+	{
+		*root = binary_tree_node(NULL, value);
+		if (!*root)
+			return (NULL);
+		(*root)->left = NULL;
+		(*root)->right = NULL;
+		return (*root);
+	}
 
-    new_index = tree_size(*root) + 1;
-    parent = node_at_index(*root, new_index / 2);
-    if (!parent)
-        return (NULL);
+	new_index = tree_size(*root) + 1;
+	parent = node_at_index(*root, new_index / 2);
+	if (!parent)
+		return (NULL);
 
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return (NULL);
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    if (new_index % 2 == 0)
-        parent->left = new_node;
-    else
-        parent->right = new_node;
+	if (new_index % 2 == 0)
+		parent->left = new_node;
+	else
+		parent->right = new_node;
 
-    return (sift_up(new_node));
+	return (sift_up(new_node));
 }
