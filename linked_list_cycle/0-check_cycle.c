9eeb956f85dae64a2ebc9005555246217d5e4192
Mano <mano19029@hotmail.fr>
2026-06-26 10:11:48 +0200

    cycle

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+    listint_t *slow = list;
+    listint_t *fast = list;
+
+        if (list == NULL)
+    {
+        return 0;
+    }
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
