cf999ea562b95680044bc89ace410a1f448eef9b
Mano <mano19029@hotmail.fr>
2026-07-10 15:12:39 +0200

    dsv

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+    
+    Args:
+        data: List of integers, each representing 1 byte (only 8 LSBits considered)
+    
+    Returns:
+        bool: True if data is valid UTF-8 encoding, False otherwise
+    """
+    remaining_bytes = 0
+    
+    for byte in data:
+        byte = byte & 0xFF
+        
+        if remaining_bytes == 0:
+            if (byte >> 5) == 0b110:
+                remaining_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                remaining_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                remaining_bytes = 3
+            elif (byte >> 7) == 0:
+                remaining_bytes = 0
+            else:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            remaining_bytes -= 1
+    return remaining_bytes == 0
