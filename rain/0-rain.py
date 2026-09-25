38113c25c50788cb394d23782508f3cf81f6c556
Maniok <mano19029@hotmail.fr>
2026-09-25 13:09:43 +0200

    qcs

---
+++ rain/0-rain.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+0_main
+"""
+
+def rain(walls):
+    total = 0
+
+    for i in range(len(walls)):
+        left = max(walls[:i], default=0)
+        right = max(walls[i+1:], default=0)
+
+        water = min(left, right) - walls[i]
+
+        if water > 0:
+            total += water
+
+    return total
