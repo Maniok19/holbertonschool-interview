d35107028623cdd73d4951d463cc93ed99b3e83e
Maniok <mano19029@hotmail.fr>
2026-09-25 10:51:27 +0200

    vdf

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -14,25 +14,17 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 	if (!list)
 		return (NULL);
-
-	/* Start from the express lane */
 	express = list->express;
 	prev = list;
-
-	/* Traverse the express lane */
 	while (express)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
 			   express->index, express->n);
-
 		if (express->n >= value)
 			break;
-
 		prev = express;
 		express = express->express;
 	}
-
-	/* Determine the range for linear search */
 	if (express)
 	{
 		printf("Value found between indexes [%lu] and [%lu]\n",
@@ -40,28 +32,21 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	}
 	else
 	{
-		/* Find the last node */
 		express = prev;
 		while (express->next)
 			express = express->next;
 		printf("Value found between indexes [%lu] and [%lu]\n",
 			   prev->index, express->index);
 	}
-
-	/* Linear search in the identified range */
 	while (prev && prev->index <= express->index)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
 			   prev->index, prev->n);
-
 		if (prev->n == value)
 			return (prev);
-
 		if (prev->n > value)
 			break;
-
 		prev = prev->next;
 	}
-
 	return (NULL);
 }
